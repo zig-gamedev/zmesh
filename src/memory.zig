fde74299751097b7a7ad24847ca042129b7a61ec
kcbanner <kcbanner@gmail.com>
2026-02-07 19:57:12 -0500

    - Move global state in GlobalState (similar to the approach used with zphysics) - Update to the new std.Io.Mutex API (requires Io)

--- src/memory.zig
+++ src/memory.zig
@@ -1,28 +1,15 @@
 const std = @import("std");
 const options = @import("zmesh_options");
 
-const SpinLock = struct {
-    state: u32 = 0,
+pub fn init(alloc: std.mem.Allocator, io: std.Io) void {
+    std.debug.assert(state == null);
 
-    fn lock(s: *SpinLock) void {
-        while (@atomicRmw(u32, &s.state, .Xchg, 1, .seq_cst) != 0) {
-            while (@atomicLoad(u32, &s.state, .acquire) != 0) {
-                std.Thread.yield() catch {};
-            }
-        }
-    }
-
-    fn unlock(s: *SpinLock) void {
-        @atomicStore(u32, &s.state, 0, .release);
-    }
-};
-
-pub fn init(alloc: std.mem.Allocator) void {
-    std.debug.assert(mem_allocator == null and mem_allocations == null);
-
-    mem_allocator = alloc;
-    mem_allocations = std.AutoHashMap(usize, usize).init(alloc);
-    mem_allocations.?.ensureTotalCapacity(32) catch unreachable;
+    state = .{
+        .io = io,
+        .mem_allocator = alloc,
+        .mem_allocations = .init(alloc),
+    };
+    state.?.mem_allocations.ensureTotalCapacity(32) catch unreachable;
 
     const zmeshMallocPtr = @extern(*?*const fn (size: usize) callconv(.c) ?*anyopaque, .{
         .name = "zmeshMallocPtr",
@@ -49,9 +36,8 @@ pub fn init(alloc: std.mem.Allocator) void {
 }
 
 pub fn deinit() void {
-    mem_allocations.?.deinit();
-    mem_allocations = null;
-    mem_allocator = null;
+    state.?.mem_allocations.deinit();
+    state = null;
 }
 
 const MallocFn = *const fn (size: usize) callconv(.c) ?*anyopaque;
@@ -62,23 +48,26 @@ extern fn meshopt_setAllocator(
     deallocate: FreeFn,
 ) void;
 
-var mem_allocator: ?std.mem.Allocator = null;
-var mem_allocations: ?std.AutoHashMap(usize, usize) = null;
-var mem_mutex: SpinLock = .{};
 const mem_alignment: std.mem.Alignment = .@"16";
+pub const GlobalState = struct {
+    io: std.Io,
+    mem_allocator: std.mem.Allocator,
+    mem_allocations: std.AutoHashMap(usize, usize),
+    mem_mutex: std.Io.Mutex = .init,
+};
+var state: ?GlobalState = null;
 
 pub fn zmeshMalloc(size: usize) callconv(.c) ?*anyopaque {
-    mem_mutex.lock();
-    defer mem_mutex.unlock();
+    state.?.mem_mutex.lockUncancelable(state.?.io);
+    defer state.?.mem_mutex.unlock(state.?.io);
 
-    const mem = mem_allocator.?.alignedAlloc(
+    const mem = state.?.mem_allocator.alignedAlloc(
         u8,
         mem_alignment,
         size,
     ) catch @panic("zmesh: out of memory");
 
-    mem_allocations.?.put(@intFromPtr(mem.ptr), size) catch @panic("zmesh: out of memory");
-
+    state.?.mem_allocations.put(@intFromPtr(mem.ptr), size) catch @panic("zmesh: out of memory");
     return mem.ptr;
 }
 
@@ -97,38 +86,38 @@ pub fn zmeshAllocUser(user: ?*anyopaque, size: usize) callconv(.c) ?*anyopaque {
 }
 
 fn zmeshRealloc(ptr: ?*anyopaque, size: usize) callconv(.c) ?*anyopaque {
-    mem_mutex.lock();
-    defer mem_mutex.unlock();
+    state.?.mem_mutex.lockUncancelable(state.?.io);
+    defer state.?.mem_mutex.unlock(state.?.io);
 
-    const old_size = if (ptr != null) mem_allocations.?.get(@intFromPtr(ptr.?)).? else 0;
+    const old_size = if (ptr != null) state.?.mem_allocations.get(@intFromPtr(ptr.?)).? else 0;
 
     const old_mem = if (old_size > 0)
         @as([*]align(mem_alignment.toByteUnits()) u8, @ptrCast(@alignCast(ptr)))[0..old_size]
     else
         @as([*]align(mem_alignment.toByteUnits()) u8, undefined)[0..0];
 
-    const mem = mem_allocator.?.realloc(old_mem, size) catch @panic("zmesh: out of memory");
+    const mem = state.?.mem_allocator.realloc(old_mem, size) catch @panic("zmesh: out of memory");
 
     if (ptr != null) {
-        const removed = mem_allocations.?.remove(@intFromPtr(ptr.?));
+        const removed = state.?.mem_allocations.remove(@intFromPtr(ptr.?));
         std.debug.assert(removed);
     }
 
-    mem_allocations.?.put(@intFromPtr(mem.ptr), size) catch @panic("zmesh: out of memory");
+    state.?.mem_allocations.put(@intFromPtr(mem.ptr), size) catch @panic("zmesh: out of memory");
 
     return mem.ptr;
 }
 
 fn zmeshFree(maybe_ptr: ?*anyopaque) callconv(.c) void {
     if (maybe_ptr) |ptr| {
-        mem_mutex.lock();
-        defer mem_mutex.unlock();
+        state.?.mem_mutex.lockUncancelable(state.?.io);
+        defer state.?.mem_mutex.unlock(state.?.io);
 
-        const try_get_allocation = mem_allocations.?.fetchRemove(@intFromPtr(ptr));
+        const try_get_allocation = state.?.mem_allocations.fetchRemove(@intFromPtr(ptr));
         if (try_get_allocation) |alloc| {
             const size = alloc.value;
             const mem = @as([*]align(mem_alignment.toByteUnits()) u8, @ptrCast(@alignCast(ptr)))[0..size];
-            mem_allocator.?.free(mem);
+            state.?.mem_allocator.free(mem);
         }
     }
 }
