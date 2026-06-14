0ca96d90c67fc3f9ff5ac1c0b221165389813691
kcbanner <kcbanner@gmail.com>
2026-06-14 00:08:32 -0400

    - Fixup deprecated std.meta.fields usage

--- build.zig
+++ build.zig
@@ -18,8 +18,11 @@ pub fn build(b: *std.Build) void {
     };
 
     const options_step = b.addOptions();
-    inline for (std.meta.fields(@TypeOf(options))) |field| {
-        options_step.addOption(field.type, field.name, @field(options, field.name));
+    inline for (
+        comptime std.meta.fieldTypes(@TypeOf(options)),
+        comptime std.meta.fieldNames(@TypeOf(options)),
+    ) |field_type, field_name| {
+        options_step.addOption(field_type, field_name, @field(options, field_name));
     }
 
     const options_module = options_step.createModule();
